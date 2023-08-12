4ea6369f619aad209cc80884cd756e8ccf485911
Austinps <austin.stack@andercore.com>
2023-08-12 17:49:36 +0200

    Sub Step

--- license_check.sh
+++ license_check.sh
@@ -4,8 +4,9 @@ TOTAL_COUNTER=0
 FAIL_COUNTER=0
 INPUT_FILE="$1"
 ALLOW_LIST="$2"
+BLOCK_LIST="$3"
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "Usage: $0 <licenses.csv> <allow_list>"
     exit 1
 fi
@@ -20,7 +21,7 @@ while IFS=',' read -r line; do
     TOTAL_COUNTER=$((TOTAL_COUNTER + 1))
 
     # Check if the license does not match the ALLOW_LIST or contains "GPL"
-    if ! [[ ${line} =~ $ALLOW_LIST ]] || [[ ${line} =~ "GPL" ]]; then
+    if ! [[ ${line} =~ $ALLOW_LIST ]] || [[ ${line} =~ $BLOCK_LIST ]]; then
         echo $line >>invalid.csv
         if ! [[ ${line} =~ "Name" ]]; then
             FAIL_COUNTER=$((FAIL_COUNTER + 1))
