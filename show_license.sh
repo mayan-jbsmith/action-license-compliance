5cf9e3bf0a3687f133ebf09ce94c683b4dc172cf
Austinps <austin.stack@andercore.com>
2023-08-12 18:23:41 +0200

    Use lists in show all script

--- show_license.sh
+++ show_license.sh
@@ -10,19 +10,19 @@ if [ $# -ne 3 ]; then
 fi
 
 {
+    LICENSE_COUNTER=0
     FAIL_COUNTER=0
-    TOTAL_COUNTER=0
 
     # Count the number of licenses that need to be researched
     while IFS=',' read -r line; do
-        TOTAL_COUNTER=$((TOTAL_COUNTER + 1))
         if ! [[ ${line} =~ $ALLOW_LIST ]] || [[ ${line} =~ $BLOCK_LIST ]]; then
             FAIL_COUNTER=$((FAIL_COUNTER + 1))
         fi
+        LICENSE_COUNTER=$((LICENSE_COUNTER + 1))
     done < <(sed 1d "$INPUT_FILE")
 
     # Display the message
-    echo "<h4>$FAIL_COUNTER / $TOTAL_COUNTER licenses need to be researched.</h4>"
+    echo "<h4>$FAIL_COUNTER / $LICENSE_COUNTER licenses need to be researched.</h4>"
     echo "<table>"
     echo "<th>#</th> <th>Status</th> <th>Name</th> <th>License Type</th>"
 } >>"$GITHUB_STEP_SUMMARY"
@@ -34,7 +34,8 @@ sed 1d "$INPUT_FILE" | while IFS=',' read -r line; do
         ICON=":red_circle:"
     fi
     if ! [[ ${line} =~ "Name" ]]; then
-        echo "<tr>" "<td>$TOTAL_COUNTER</td>" "<td>$ICON</td>" "<td><a href=\"$(echo "$line" | awk -F '\"' '{print $6}')\" target=\"_blank\">$(echo "$line" | awk -F '\"' '{print $2}')</a></td>" "<td>$(echo "$line" | awk -F '\"' '{print $4}')</td>" "</tr>" >>"$GITHUB_STEP_SUMMARY"
+        echo "<tr>" "<td>$LICENSE_COUNTER</td>" "<td>$ICON</td>" "<td><a href=\"$(echo "$line" | awk -F '\"' '{print $6}')\" target=\"_blank\">$(echo "$line" | awk -F '\"' '{print $2}')</a></td>" "<td>$(echo "$line" | awk -F '\"' '{print $4}')</td>" "</tr>" >>"$GITHUB_STEP_SUMMARY"
+        LICENSE_COUNTER=$((LICENSE_COUNTER - 1))
     fi
 done
 
