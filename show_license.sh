3e9c12210d596199f3aed52a61de73fa7b3ec125
Austinps <austin.stack@andercore.com>
2023-08-12 18:46:40 +0200

    Add common script for shared functionality

--- show_license.sh
+++ show_license.sh
@@ -10,8 +10,6 @@ process_licenses "$INPUT_FILE" "$ALLOW_LIST" "$BLOCK_LIST"
 
 {
     LICENSE_COUNTER=0
-
-    # Display the message
     echo "<table>"
     echo "<th>#</th> <th>Status</th> <th>Name</th> <th>License Type</th>"
 } >>"$GITHUB_STEP_SUMMARY"
@@ -23,8 +21,8 @@ sed 1d "$INPUT_FILE" | while IFS=',' read -r line; do
         ICON=":red_circle:"
     fi
     if ! [[ ${line} =~ "Name" ]]; then
+        LICENSE_COUNTER=$((LICENSE_COUNTER + 1))
         echo "<tr>" "<td>$LICENSE_COUNTER</td>" "<td>$ICON</td>" "<td><a href=\"$(echo "$line" | awk -F '\"' '{print $6}')\" target=\"_blank\">$(echo "$line" | awk -F '\"' '{print $2}')</a></td>" "<td>$(echo "$line" | awk -F '\"' '{print $4}')</td>" "</tr>" >>"$GITHUB_STEP_SUMMARY"
-        LICENSE_COUNTER=$((LICENSE_COUNTER - 1))
     fi
 done
 
